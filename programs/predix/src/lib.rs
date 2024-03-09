82fe17fc84b484ceda0171a73f765aa00fd9d70a
asimnecip <necipasimarslan@gmail.com>
2024-03-09 17:38:12 +0300

    create_post function added

--- programs/predix/src/lib.rs
+++ programs/predix/src/lib.rs
@@ -1,15 +1,98 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::system_program;
+use borsh::{BorshDeserialize, BorshSerialize};
 
-declare_id!("Apkfrh7Bo1UegJLS78fNsgpunbh5bA6LmZSpKVHJXSN6");
+declare_id!("FWp2NxpzgK235TFK1HE96rbVNtXZ8U2oxwXMn3ptwu1A");
 
 #[program]
-pub mod predix {
+pub mod rehelloanchor {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_post(
+        ctx: Context<CreatePost>,
+        topic: String,
+        content: String,
+        post_end_date: i64,
+    ) -> Result<()> {
+        let post: &mut Account<Post> = &mut ctx.accounts.post;
+        let author: &Signer = &ctx.accounts.author;
+        let clock = Clock::get().unwrap();
+
+        // Char limit for strings
+        if topic.chars().count() > 50 {
+            return Err(ErrorCode::TopicTooLong.into());
+        }
+        if content.chars().count() > 280 {
+            return Err(ErrorCode::ContentTooLong.into());
+        }
+
+        post.author = author.key();
+        post.post_date = clock.unix_timestamp;
+        post.post_end_date = post_end_date;
+        post.topic = topic;
+        post.content = content;
+        post.post_init_bet = 0;
+        post.post_bet_pool = 0;
+        post.post_finished = false;
+
+        post.post_participants.push(author.key());
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreatePost<'info> {
+    #[account(init, payer = author, space = Post::LEN)]
+    pub post: Account<'info, Post>,
+    #[account(mut)]
+    pub author: Signer<'info>,
+    #[account(address = system_program::ID)]
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub system_program: AccountInfo<'info>,
+    //ppub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Post {
+    pub author: Pubkey,                 //Post author
+    pub post_date: i64,                 //Post publish date
+    pub post_end_date: i64,             //Post end date
+    pub topic: String,                  //Post topic
+    pub content: String,                //Post content
+    pub post_init_bet: u64,             //Post init bet
+    pub post_bet_pool: u64,             //Post bet pool
+    pub post_finished: bool,            //Post finished?
+    pub post_participants: Vec<Pubkey>, //Post participants
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4;
+const MAX_TOPIC_LENGTH: usize = 50 * 4;
+const MAX_CONTENT_LENGTH: usize = 280 * 4;
+const POST_BET_LENGTH: usize = 8;
+const POST_FINISHED_LENGTH: usize = 1;
+const POST_PARTICIPANTS_LENGTH: usize = 32 * 10;
+
+impl Post {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH // Author.
+        + TIMESTAMP_LENGTH // Timestamp.
+        + TIMESTAMP_LENGTH // Timestamp.
+        + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH // Topic.
+        + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH // Content.
+        + POST_BET_LENGTH // Post date.
+        + POST_BET_LENGTH // Post bet pool.
+        + POST_FINISHED_LENGTH // Post status.
+        + POST_PARTICIPANTS_LENGTH;
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided topic should be 50 characters long maximum.")]
+    TopicTooLong,
+    #[msg("The provided content should be 280 characters long maximum.")]
+    ContentTooLong,
+}
